abd96dd7c7f81dfcb4582561acc12d8b983e25a1
Angelo Sarto <angelo.sarto@bluejartech.com>
2015-11-10 03:19:14 -0600

    updated to show error messages for login

--- jwt-test/app/controllers/login.js
+++ jwt-test/app/controllers/login.js
@@ -2,12 +2,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  login_progress: '',
+  login_error: '',
+
   actions: {
     authenticate: function() {
+      this.set('login_progress', 'in_progress');
+      this.set('login_error', '');
       var credentials = this.getProperties('identification', 'password'),
         authenticator = 'simple-auth-authenticator:jwt';
 
-      this.get('session').authenticate(authenticator, credentials);
-    }
-  }
+      this.get('session').authenticate(authenticator, credentials)
+        .catch((reason) => {
+        console.warn('authentication failed: ' + ' ' + reason.error + ' ' +reason.msg + ' ' + reason);
+      this.set('login_progress', '');
+      if (reason.error === 'authn_user_pass_fail') {
+        this.set('login_error', 'username and password not valid, please try again');
+      }
+      else {
+        this.set('login_error', 'could not contact server - if this persists please contact support');
+      }
+    });
+}
+}
 });
